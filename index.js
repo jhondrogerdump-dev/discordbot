caf29f6c53c0a853a48676d4e8dc828294cacef1
shiro9 <jhondrogerdump@gmail.com>
2026-04-17 03:47:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ client.once("ready", async () => {
   if (sent) return;
   sent = true;
 
-  const channel = await client.channels.fetch("YOUR_CHANNEL_ID");
+  const channel = await client.channels.fetch("1493379919294627933");
 
   const container = new ContainerBuilder()
     .addTextDisplayComponents(
