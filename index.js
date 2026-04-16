1731f78c6e446037129d2ef405c04b44b19fd644
shiro9 <jhondrogerdump@gmail.com>
2026-04-17 07:36:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const {
   SeparatorSpacingSize
 } = require("discord.js");
 
-// ✅ CLIENT MUST BE FIRST FIX
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -16,52 +15,45 @@ const client = new Client({
   ]
 });
 
-// ================= READY EVENT =================
 client.once("ready", async () => {
   console.log(`${client.user.tag} is online`);
 
-  // wait for Railway + Discord sync
   setTimeout(async () => {
     try {
       const channel = await client.channels.fetch("1493379919294627933");
-
       if (!channel) return console.log("Channel not found");
 
-      // ===== CONTAINER MESSAGE =====
       const container = new ContainerBuilder()
         .addTextDisplayComponents(
-          new TextDisplayBuilder().setContent("**Script loader
-                                              -# copy for pc**")
+          new TextDisplayBuilder().setContent("**Script loader**\n-# copy for pc")
         )
         .addSeparatorComponents(
           new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
         )
         .addTextDisplayComponents(
-          new TextDisplayBuilder().setContent("```loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/7d7489f4b9e04ff7"))()```")
-        );
-          .addSeparatorComponents(
-          new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
+          new TextDisplayBuilder().setContent(
+            "```loadstring(game:HttpGet(\"https://vss.pandadevelopment.net/virtual/file/7d7489f4b9e04ff7\"))()```"
+          )
         )
-          await channel.send({
-  components: [
-    container,
-    {
-      type: 1,
-      components: [
-        {
-          type: 2,
-          style: 5,
-          label: "supported games",
-          url: "https://discord.com/channels/1491419835039612958/1492584777881227515"
-        }
-      ]
-    }
-  ],
-  flags: 1 << 15
-});
+        .addSeparatorComponents(
+          new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small)
+        );
 
       await channel.send({
-        components: [container],
+        components: [
+          container,
+          {
+            type: 1,
+            components: [
+              {
+                type: 2,
+                style: 5,
+                label: "supported games",
+                url: "https://discord.com/channels/1491419835039612958/1492584777881227515"
+              }
+            ]
+          }
+        ],
         flags: 1 << 15
       });
 
@@ -73,5 +65,4 @@ client.once("ready", async () => {
   }, 3000);
 });
 
-// ================= LOGIN =================
 client.login(process.env.TOKEN);
